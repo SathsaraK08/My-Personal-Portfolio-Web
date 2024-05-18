bb8dd014c4a79af53069d463b3acf81a56eac5a5
Sathsara Karunarathne <79186512+RomiS08@users.noreply.github.com>
2024-05-18 22:27:55 +0530

    Connected AWS RDS

--- include/db.php
+++ include/db.php
@@ -1,11 +1,19 @@
 <?php
-$db = mysqli_connect(
-    "localhost",
-    "root",
-    "Romi.5351",
-    "iportfolio"
-);
+// $db = mysqli_connect(
+//     "localhost",
+//     "root",
+//     "Romi.5351",
+//     "iportfolio"
+// );
 
+
+// Production Environment - AWS RDS Remote MySQL Database Connection Configuration
+
+$host = 'mysql.cdm08uoa8kk2.us-east-1.rds.amazonaws.com';
+$db = 'iportfolio';
+$user = 'admin';
+$pass = 'Romi.5351';
+$charset = 'utf8mb4';
 //if($db){
 //    echo "databse is connected !";
 //}else{
